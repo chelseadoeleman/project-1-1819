fccb67a1c5124cab5ee6b776f8bcc5e856bcfb90
Chelsea <chelsea.doeleman@hva.nl>
2019-03-05 11:53:46 +0100

    cleaned data

---
+++ client/src/js/helpers.js
@@ -0,0 +1,76 @@
+
+const getAuthorFromResult = (result) => {
+    return result.authors
+        && result.authors["main-author"]
+        && result.authors["main-author"]._attributes
+        && result.authors["main-author"]._attributes["search-term"]
+        || undefined
+}
+
+const getImageFromResult = (result) => {
+    return result.coverimages
+        && result.coverimages.coverimage
+        &&  result.coverimages.coverimage[1]
+        &&  result.coverimages.coverimage[1]._text
+        || undefined
+}
+
+const getSeriesFromResult = (result) => {
+    return result.series
+        && result.series["series-title"]
+        && result.series["series-title"]._text
+        || undefined
+}
+
+const getTitleFromResult = (result) => {
+    return result.titles
+        && result.titles["short-title"]
+        && result.titles["short-title"]._text
+        || undefined
+}
+
+const getFormatFromResult = (result) => {
+    return result.formats
+        && result.formats.format
+        && result.formats.format._text
+        || undefined
+}
+
+const getSubjectFromResult = (result) => {
+    return result.subjects
+        && result.subjects["topical-subject"]
+        && result.subjects["topical-subject"]._text
+        || undefined
+}
+
+const getTargetAudienceFromResult = (result) => {
+    return result["target-audiences"]
+        && result["target-audiences"]["target-audience"]
+        && result["target-audiences"]["target-audience"]._text
+        || undefined
+}
+
+const getSummaryFromResult = (result) => {
+    return result.summaries
+        && result.summaries.summary
+        && result.summaries.summary._text
+        || undefined
+}
+
+
+export const getTransformedResultFromResults = (results) => {
+    return results
+        ? results.map(result => ({
+            author: getAuthorFromResult(result),
+            image: getImageFromResult(result),
+            series: getSeriesFromResult(result),
+            title: getTitleFromResult(result),
+            format: getFormatFromResult(result),
+            subject: getSubjectFromResult(result),
+            targetAudience: getTargetAudienceFromResult(result),
+            summary: getSummaryFromResult(result)
+        })) 
+        : []
+}
+
+
--- client/src/js/script.js
+++ client/src/js/script.js
@@ -1,1 +1,27 @@
+'use strict'
 
+import 'babel-polyfill'
+import { API } from '../../../node_modules/oba-wrapper/js/index'
+import { getTransformedResultFromResults } from './helpers'
+
+(async () => {
+    try {
+    const api = new API({
+        key: "1e19898c87464e239192c8bfe422f280"
+    })
+
+    const stream = await api.createStream("search/harry potter{5}")
+        stream
+            .pipe(getTransformedResultFromResults)
+            .pipe(console.log)
+            // .pipe(toJSON)
+            // .pipe(cleanJSON)
+            // .pipe(renderToDocument)
+
+        // const transformedResults = getTransformedResultFromResults(results)
+        // console.log(transformedResults)
+
+    } catch (error) {
+        throw new Error(error)
+    }
+})()
