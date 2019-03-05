d3f0b08a27f2fbf335957e364a06a389e303758e
Chelsea <chelsea.doeleman@hva.nl>
2019-03-05 16:26:23 +0100

    requesting multiple facets

--- client/src/js/script.js
+++ client/src/js/script.js
@@ -7,21 +7,39 @@ import { getTransformedResultFromResults } from './helpers'
 (async () => {
     try {
     const api = new API({
-        key: "1e19898c87464e239192c8bfe422f280"
+        key: '1e19898c87464e239192c8bfe422f280'
     })
 
-    const stream = await api.createStream("search/harry potter{5}")
+    const stream = await api.createStream('search/targetaudience:ageyouth&facet=type(book)&facet=language(dut){5}')
         stream
             .pipe(getTransformedResultFromResults)
+            .pipe(handleRender)
             .pipe(console.log)
-            // .pipe(toJSON)
-            // .pipe(cleanJSON)
-            // .pipe(renderToDocument)
-
-        // const transformedResults = getTransformedResultFromResults(results)
-        // console.log(transformedResults)
 
     } catch (error) {
         throw new Error(error)
+    }
+
+    function handleRender(results) {
+        results
+            .filter(result => {
+                return result.image && result.image.includes('https://v19.')
+            })
+            .forEach(result => {
+                const { image } = result
+                console.log(image)
+                const section = document.querySelector('section')
+                section.classList.add('book-wrapper')
+                const article = document.createElement('article')
+                const img = document.createElement('img')
+                if (image === undefined) {
+                    return
+                } else {
+                    img.setAttribute('src', image)
+
+                    article.appendChild(img)
+                    section.appendChild(article)
+                }
+        })
     }
 })()
